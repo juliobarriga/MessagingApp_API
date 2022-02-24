6d05081f362f021b817605124b256d90c3512c46
julio <juliodbarriga@gmail.com>
2022-02-24 17:09:46 -0600

    Modified LoginRequest Getters.

--- src/main/java/com/example/messaging_api/model/Request/LoginRequest.java
+++ src/main/java/com/example/messaging_api/model/Request/LoginRequest.java
@@ -2,13 +2,8 @@ package com.example.messaging_api.model.Request;
 
 public class LoginRequest {
 
-    private String phoneNumber;
     private String userName;
     private String password;
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
 
     public String getUserName() {
         return userName;
