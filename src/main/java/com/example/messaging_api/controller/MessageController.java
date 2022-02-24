7e271f47e9c3a1a5f674fc1397970225ffd131f0
julio <juliodbarriga@gmail.com>
2022-02-24 13:51:29 -0600

    Created MessageController with endpoints.

---
+++ src/main/java/com/example/messaging_api/controller/MessageController.java
@@ -0,0 +1,46 @@
+package com.example.messaging_api.controller;
+
+import com.example.messaging_api.model.Message;
+import com.example.messaging_api.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+
+    private MessageService messageService;
+
+    @Autowired
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getAllMessages(){
+        return messageService.getAllMessages();
+    }
+
+    @PostMapping("/messages/{receiverId}")
+    public Message createMessageByReceiverId(@PathVariable(value = "receiverId") Long receiverId, @RequestBody Message messageObject){
+        return messageService.createMessageByReceiverId(receiverId, messageObject);
+    }
+
+    @GetMapping("/messages/{receiverId}")
+    public List<Message> getMessagesByReceiverId(@PathVariable(value = "receiverId") Long receiverId){
+        return messageService.getMessagesByReceiverId(receiverId);
+    }
+
+    @PutMapping("/messages/{messageId}")
+    public Message updateMessage(@PathVariable(value = "messageId") Long messageId){
+        return messageService.updateMessage(messageId);
+    }
+
+    @DeleteMapping("/messages/{messageId}")
+    public Message deleteMessage(@PathVariable(value = "messageId") Long messageId){
+        return messageService.deleteMessage(messageId);
+    }
+
+}
