38cb495952a4123e97171b565fd272c8d1b8c4cb
julio <juliodbarriga@gmail.com>
2022-02-24 20:05:26 -0600

    Added and commented out find methods.

--- src/main/java/com/example/messaging_api/repository/MessageRepository.java
+++ src/main/java/com/example/messaging_api/repository/MessageRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByReceiverId(Long receiverId);
+//    List<Message> findByReceiverId(Long receiverId);
+    List<Message> findBySenderIdOrReceiverId(Long senderId, Long receiverId);
+    List<Message> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
+    Optional<Message> findByIdAndSenderId(Long messageId, Long senderId);
 }
