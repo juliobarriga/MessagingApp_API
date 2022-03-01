08b51b7b880b2e7009fa9ff6810197cb7d036710
julio <juliodbarriga@gmail.com>
2022-03-01 01:06:41 -0600

    another try CORS.

--- src/main/java/com/example/messaging_api/MessagingApiApplication.java
+++ src/main/java/com/example/messaging_api/MessagingApiApplication.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://messagingapp--api.herokuapp.com")
 public class MessagingApiApplication {
 
     public static void main(String[] args) {
