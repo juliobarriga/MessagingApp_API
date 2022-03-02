10d97d9b0d273863ba0c7919ad2edb125d0359cc
julio <juliodbarriga@gmail.com>
2022-03-02 15:29:36 -0600

    Changes url for cors.

--- src/main/java/com/example/messaging_api/MessagingApiApplication.java
+++ src/main/java/com/example/messaging_api/MessagingApiApplication.java
@@ -35,7 +35,7 @@ public class MessagingApiApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://messaging--app.herokuapp.com"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
